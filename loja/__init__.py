8a694781a9f9b4a90e5b26b7c1d51a4447ad3e53
Anderson0312 <andersonmoura812@gmail.com>
2022-05-14 00:09:47 -0300

    carr

--- loja/__init__.py
+++ loja/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
-from flask_uploads import IMAGES, UploadSet, configure_uploads, patch_request_class
+from flask_uploads import IMAGES, UploadSet, configure_uploads
 import os
 
 
@@ -16,7 +16,7 @@ app.config['UPLOADED_PHOTOS_DEST'] = os.path.join(basedir, 'static/images')
 
 photos = UploadSet('photos', IMAGES)
 configure_uploads(app, photos)
-patch_request_class(app)
+
 
 
 from loja.admin import rotas
--- loja/carrinho/carrinhos.py
+++ loja/carrinho/carrinhos.py
@@ -13,28 +13,28 @@ def M_Dicionarios(dic1, dic2):
         return dict(list(dic1.items()) + list(dic2.items()))
     return False
 
+
+
     # """Função para ADCIONAR uma MARCA da loja"""
 
-@app.route('/addCart', methods=['POST'])
+@app.route('/addcart', methods=['POST'])
 def AddCart():
-    try:   
+    try:
         produto_id = request.form.get('produto_id')
-        quantity = request.form.get('quantity')
+        quantity = int(request.form.get('quantity'))
         colors = request.form.get('colors')
         produto = Addproduto.query.filter_by(id=produto_id).first()
 
-        if produto_id and quantity and colors and request.method == 'POST':
-            DicItems = {produto_id:{'Nome':produto.name, 'Price':produto.price, 'Disconto':produto.discount,
-                                    'Cor':produto.colors, 'Quantidade':produto.quantity, 'Img':produto.image_1,
-                                    }}
+        if produto_id and quantity and colors and request.method == "POST":
+            DicItems = {produto_id:{'name':produto.name, 'price':float(produto.price), 'discount':produto.discount, 'color':colors, 'quantidade':quantity, 'img':produto.image_1, 'colors':produto.colors}}
             if 'LojainCarrinho' in session:
                 print(session["LojainCarrinho"])
+
                 if produto_id in session['LojainCarrinho']:
                     print('Este produto ja existe no carrinho')
                 else:
                     session['LojainCarrinho'] = M_Dicionarios(session['LojainCarrinho'], DicItems)
                     return redirect(request.referrer)
-
             else:
                 session['LojainCarrinho'] = DicItems
                 return redirect(request.referrer)
@@ -43,3 +43,64 @@ def AddCart():
         print(e)
     finally:
         return redirect(request.referrer)
+
+
+@app.route('/carros')
+def getCart():
+    if 'LojainCarrinho' not in session or len(session['LojainCarrinho']) <= 0:
+        return redirect(url_for('home'))
+    subtotal = 0
+    valorpagar = 0
+    for key, produto in session['LojainCarrinho'].items():
+        discount = (produto['discount']/100) * float(produto['price'])
+        subtotal += float(produto['price']) * int(produto['quantidade'])
+        subtotal -= discount
+        imposto = ("%.2f" % (.06 * float(subtotal)))
+        valorpagar = float('%.2f' % (1.06 * subtotal))
+    return render_template('produtos/carros.html', imposto=imposto, valorpagar=valorpagar)
+
+@app.route('/updateCarro/<int:code>', methods=['POST'])
+def updateCarro(code):
+    if 'LojainCarrinho' not in session or len(session['LojainCarrinho']) <= 0:
+        return redirect(url_for('home'))
+    if request.method == 'POST':
+        quantity = request.form.get('quantidade')
+        color = request.form.get('color')
+        try:
+            session.modified = True
+            for key, item in session['LojainCarrinho'].items():
+                if int(key) == code:
+                    item['quantidade'] = quantity
+                    item['color'] = color
+                    flash('ITEM FOI ATUALIZADO COM SUCESSO')
+                    return redirect(url_for('getCart'))
+
+        except Exception as e:
+            print(e)
+            return redirect(url_for('getCart'))
+
+
+@app.route('/deleteitem/<int:id>')
+def deleteitem(id):
+    if 'LojainCarrinho' not in session or len(session['LojainCarrinho']) <= 0:
+        return redirect(url_for('home'))
+    try:
+        session.modified = True
+        for key, item in session['LojainCarrinho'].items():
+            if int(key) == id:
+                session['LojainCarrinho'].pop(key,None)
+                flash('ITEM FOI DELETADO COM SUCESSO')
+                return redirect(url_for('getCart'))
+
+    except Exception as e:
+        print(e)
+        return redirect(url_for('getCart'))
+
+
+@app.route('/vazio')
+def vazio_Cart():
+    try:
+        session.clear()
+        return redirect(url_for('home'))
+    except Exception as e:
+        print(e)
